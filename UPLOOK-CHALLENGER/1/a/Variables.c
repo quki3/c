01973d77dbfe631ce61ab39903ac748ea2552af7
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-06-29 09:37:29 -0300

    Create Variables.c

---
+++ UPLOOK-CHALLENGER/1/a/Variables.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+    int myInteger = 42;
+    printf("The value of myInteger is: %d\n", myInteger);
+    return 0;
+}
+
+/* README
+          //this command you need run in terminal that this do is create a executable file
+          gcc -o integer_example integer_example.c
+          // run the commpiled program
+          ./name_of_executable.
+*/
+
+
+
