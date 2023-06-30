78a5af48359c2d8dc5327d6618817aeae6a33161
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-06-30 05:29:19 -0300

    Update Variables.c

--- UPLOOK-CHALLENGER/1/a/Variables.c
+++ UPLOOK-CHALLENGER/1/a/Variables.c
@@ -1,7 +1,11 @@
-
+// #include - the contents of that file are essentially copied and pasted into the source file at the location of the #include.
+// <stdio.h>- <> This form is used for including standard library header files for your file use "".
 #include <stdio.h>
 
+// int- return type indicates that the main() function should return an integer value. 
+// main() - is the entry point of a C program. It is the function from where the program execution begins
 int main() {
+    // myInterger - name or variable
     int myInteger = 42;
     printf("The value of myInteger is: %d\n", myInteger);
     return 0;
